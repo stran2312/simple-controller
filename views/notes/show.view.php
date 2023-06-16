863acc5fc39f4735ca252278e925ff950cbe4981
Sang Tran <100749058+stran2312@users.noreply.github.com>
2023-06-15 23:46:02 -0500

    Add files via upload

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -9,6 +9,10 @@
         </p>
 
         <p><?= htmlspecialchars($note['body']) ?></p>
+
+        <footer class="mt-6">
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
+        </footer>
     </div>
 </main>
 
